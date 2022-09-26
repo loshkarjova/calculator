f21cc262286299ea0785c061ba6193b5030d80c3
ilyaxvgreat <ilyaxvgreat@gmail.com>
2022-09-26 12:46:47 +0300

    Main

---
+++ src/calc/Main.java
@@ -0,0 +1,24 @@
+package calc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Calculator for base calculations");
+        System.out.println("Version 1.0");
+        int a = 8;
+        int b = 2;
+        int c = add(a, b);
+        System.out.println(c);
+        int d = multiply(a, b);
+        System.out.println(d);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+
